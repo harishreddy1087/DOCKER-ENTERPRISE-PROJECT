65820040ae2b7292d0a80e03fdaea9d8cd0a18a3
harishreddy1087 <123229513+harishreddy1087@users.noreply.github.com>
2026-01-14 01:14:07 +0530

    Delete DOCKER-PRACTICE directory

--- DOCKER-PRACTICE/level1/server.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-  res.end('Hello reddy from inside the Docker Container!');
-});
-
-server.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
--- DOCKER-PRACTICE/multi-stage/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-    fmt.Println("Hello! This is a secure, multi-stage Go binary running on Alpine!")
-}
